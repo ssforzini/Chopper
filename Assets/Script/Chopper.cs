8fcfdd59dc9b2def7a0c8a5ac6678ff365d553db
ssforzini <sebasforzini@hotmail.com>
2017-05-09 22:23:43 -0300

    Commit Inicial
    Primer commit falta project settings

---
+++ Assets/Script/Chopper.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Chopper : MonoBehaviour {
+
+	[SerializeField]private float movementSpeed;
+	[SerializeField]private float rotationSpeed;
+	[SerializeField]private float rotorSpeed;
+	[SerializeField]private float backRotorSpeed;
+
+	void Update () {
+
+		transform.FindChild ("rotor").Rotate (Vector3.forward * Time.deltaTime * rotorSpeed);
+		transform.FindChild ("rear rotor").Rotate (Vector3.forward * Time.deltaTime * backRotorSpeed);
+		transform.FindChild ("rear rotor.001").Rotate (Vector3.back * Time.deltaTime * backRotorSpeed);
+		if(Input.GetKey(KeyCode.UpArrow)){
+			transform.Translate (Vector3.back * Time.deltaTime * movementSpeed);
+		}
+		if(Input.GetKey(KeyCode.DownArrow)){
+			transform.Translate (Vector3.forward * Time.deltaTime * movementSpeed);
+		}
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.Rotate (Vector3.down * Time.deltaTime * rotationSpeed);
+		}
+		if(Input.GetKey(KeyCode.RightArrow)){
+			transform.Rotate (Vector3.up * Time.deltaTime * rotationSpeed);
+		}
+		if(Input.GetKey(KeyCode.A)){
+			transform.Translate (Vector3.up * Time.deltaTime * movementSpeed);
+		}
+		if(Input.GetKey(KeyCode.S)){
+			transform.Translate (Vector3.down * Time.deltaTime * movementSpeed);
+		}
+	}
+}
---
+++ Assets/Script/Fire.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Space)){
+			RaycastHit hit;
+
+			Debug.DrawRay (transform.position, transform.forward * 200f);
+			/*if(Physics.Raycast(transform.position,transform.right,out hit)){
+				hit.rigidbody.AddForceAtPosition (transform.right * 10f, hit.point, ForceMode.Impulse);
+			}*/
+		}
+	}
+}
